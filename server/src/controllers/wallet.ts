586a06fc7c054dab1d0218782e29d1bb97974894
Arif Damar <arif660damar@gmail.com>
2021-12-07 03:35:09 +0300

    add get route of wallet

--- server/src/controllers/wallet.ts
+++ server/src/controllers/wallet.ts
@@ -63,4 +63,24 @@ export class WalletController {
         }
     }
 
+    public async Get(request: Request, response: Response) {
+        try {
+            console.log(response.locals.user.walletId);
+            const wallet = await Wallet
+                .findById(response.locals.user.walletId)
+                .populate({
+                    path: "cryptos",
+                    populate: {
+                        path: "exchange",
+                        select: "-symbols -createdAt -updatedAt"
+                    }
+                });
+
+            return response.status(200).send(new SuccessResult("Wallet Fetched Successfully!", wallet));
+        } catch (error) {
+            console.log(error);
+            return response.status(500).json(new FailureResult("Something went wrong."));
+        }
+    }
+
 }
--- server/src/routes/api/wallet.ts
+++ server/src/routes/api/wallet.ts
@@ -6,5 +6,6 @@ const router = express.Router();
 const walletController = new WalletController();
 
 router.post("/add", Auth.authenticate, walletController.AddCrypto);
+router.get("/", Auth.authenticate, walletController.Get);
 
 export default router;
