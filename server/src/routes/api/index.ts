457a3d504bb10f51c59a80dff86778ab1c335d76
Arif Damar <arif.damar@ogr.sakarya.edu.tr>
2022-02-02 13:28:20 +0300

    add redirect test

--- server/src/routes/api/index.ts
+++ server/src/routes/api/index.ts
@@ -20,6 +20,44 @@ router.get("/test", (req: express.Request, res: express.Response) => {
   return res.status(response).json(new SuccessResult("API is up and running!", null));
 });
 
+router.get("/redirect", async (req: express.Request, res: express.Response) => {
+  console.log("redirect");
+
+  return res.redirect("redirectA");
+});
+
+router.get("/redirectA", async (req: express.Request, res: express.Response) => {
+  console.log("redirectA");
+
+  return res.redirect("redirectB");
+});
+
+router.get("/redirectB", async (req: express.Request, res: express.Response) => {
+  console.log("redirectB");
+
+  return res.redirect("redirectC");
+});
+
+router.get("/redirectC", async (req: express.Request, res: express.Response) => {
+  console.log("redirectC");
+
+  await sleep(10000);
+
+  return res.redirect("redirectD");
+});
+
+router.get("/redirectD", async (req: express.Request, res: express.Response) => {
+  console.log("redirectD");
+
+  return res.status(200).send();
+});
+
+function sleep(ms: number) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
 router.use("/auth", authRoutes);
 router.use("/wallet", walletRoutes);
 router.use("/exchange", exchangeRoutes);
