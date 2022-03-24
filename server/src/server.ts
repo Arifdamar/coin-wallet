c42e4be958877de62db354a4856d3d73628457d5
Arif Damar <arif.damar@ogr.sakarya.edu.tr>
2022-03-24 15:51:23 +0300

    add test tcp server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,25 @@
 import { Api } from "./app";
 import { config } from "./config";
+import net from "net";
 
 Api.RunApp().then(async (app) => {
-    app.listen(config.PORT, () => {
-        console.log(`Server is running at ${config.PORT}`);
-    });
-})
+  app.listen(config.PORT, () => {
+    console.log(`Server is running at ${config.PORT}`);
+  });
+});
+
+var server = net.createServer();
+
+server.on("connection", (socket) => {
+  const remoteAddress = socket.remoteAddress + ":" + socket.remotePort;
+  console.log("New client connection", remoteAddress);
+
+  socket.on("data", (data) => {
+    console.log("Received " + data.toString() + " from client");
+    socket.write(data);
+  });
+});
+
+server.listen(9000, () => {
+  console.log("Listening on port 9000", server.address());
+});
